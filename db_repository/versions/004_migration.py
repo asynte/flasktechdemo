f5211c5717a6fc28e699ff48d761a8ef010a25ab
Adam Bolding-Jones <anb1@sfu.ca>
2016-03-02 21:49:22 -0800

    pre merge commit

--- db_repository/versions/004_migration.py
+++ db_repository/versions/004_migration.py
@@ -5,17 +5,12 @@ from migrate import *
 from migrate.changeset import schema
 pre_meta = MetaData()
 post_meta = MetaData()
-user = Table('user', pre_meta,
-    Column('id', INTEGER, primary_key=True, nullable=False),
-    Column('nickname', VARCHAR(length=64)),
-    Column('email', VARCHAR(length=120)),
-)
-
 users = Table('users', post_meta,
     Column('id', Integer, primary_key=True, nullable=False),
     Column('social_id', String(length=64), nullable=False),
     Column('userID', String(length=64), nullable=False),
     Column('email', String(length=64)),
+    Column('pwdhash', String(length=54)),
 )
 
 
@@ -24,13 +19,11 @@ def upgrade(migrate_engine):
     # migrate_engine to your metadata
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    pre_meta.tables['user'].drop()
-    post_meta.tables['users'].create()
+    post_meta.tables['users'].columns['pwdhash'].create()
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
-    pre_meta.tables['user'].create()
-    post_meta.tables['users'].drop()
+    post_meta.tables['users'].columns['pwdhash'].drop()
